6ac71c929e14fb2eca8b6f21c57e1b54e0f18792
SpazzStuff <36232417+SpazzStuff@users.noreply.github.com>
2022-09-27 13:05:29 -0400

    Product unit tests added and pass

--- estore-api/src/test/java/com/estore/api/estoreapi/model/ProductTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/model/ProductTest.java
@@ -1,8 +1,50 @@
 package com.estore.api.estoreapi.model;
 
-import org.junit.jupiter.api.Tag;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The unit test suite for the Product class
+ */
 @Tag("Model-tier")
 public class ProductTest {
-    
+    Product product;
+
+    @BeforeEach
+    public void setupProduct() {
+        float price = (float) 212.5;
+        product = new Product(5, "Charlie", "dog", "yellow", 7, price, "A friendly yellow lab.");
+    }
+
+    @Test
+    public void testGetId() {
+        assertEquals(5, product.getId());
+    }
+    @Test
+    public void testGetName() {
+        assertEquals("Charlie", product.getName());
+    }
+    @Test
+    public void testGetSpecies() {
+        assertEquals("dog", product.getSpecies());
+    }
+    @Test
+    public void testGetColor() {
+        assertEquals("yellow", product.getColor());
+    }
+    @Test
+    public void testGetAge() {
+        assertEquals(7, product.getAge());
+    }
+    @Test
+    public void testGetPrice() {
+        assertEquals(212.50, product.getPrice());
+    }
+    @Test
+    public void testGetDescription() {
+        assertEquals("A friendly yellow lab.", product.getDescription());
+    }
 }

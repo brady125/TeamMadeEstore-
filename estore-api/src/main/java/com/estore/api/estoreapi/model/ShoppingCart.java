d56571ab1a6be89170a4827e6c32a20e09d97f93
unknown <bhm6652@rit.edu>
2022-10-02 18:25:53 -0400

    shopping cart

--- estore-api/src/main/java/com/estore/api/estoreapi/model/ShoppingCart.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/ShoppingCart.java
@@ -1,5 +1,48 @@
 package com.estore.api.estoreapi.model;
 
-public class ShoppingCart {
+import java.util.ArrayList;
+import java.util.logging.Logger;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ShoppingCart {
+    private static final Logger LOG = Logger.getLogger(ShoppingCart.class.getName());
+    static final String STRING_FORMAT = "ShoppingCart [product count=%d]";
+    @JsonProperty("products")
+    private Product[] products;
+
+    public ShoppingCart(@JsonProperty("products") Product[] products) {
+        this.products = products;
+    }
+
+    public Product[] getProducts() {
+        return products;
+    }
+
+    public void addProduct(Product product) {
+        ArrayList<Product> productList = new ArrayList<>();
+        for (Product prod : products) {
+            productList.add(prod);
+        }
+        productList.add(product);
+        Product[] productArray = new Product[productList.size()];
+        productList.toArray(productArray);
+        products = productArray;
+    }
+
+    public void removeProduct(Product product) {
+        ArrayList<Product> productList = new ArrayList<>();
+        for (Product prod : products) {
+            productList.add(prod);
+        }
+        productList.remove(product);
+        Product[] productArray = new Product[productList.size()];
+        productList.toArray(productArray);
+        products = productArray;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(STRING_FORMAT, products.length);
+    }
 }

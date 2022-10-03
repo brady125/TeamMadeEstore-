bca3402e0da3cf2d4bbd84e8505138cb2708e851
SpazzStuff <36232417+SpazzStuff@users.noreply.github.com>
2022-10-03 19:23:11 -0400

    createAccount in component done

--- estore-ui/src/app/sign-up/sign-up.component.ts
+++ estore-ui/src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -9,29 +10,44 @@ export class SignUpComponent implements OnInit {
   errorMessage = "";
   display = "none";
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
   }
 
-  createAccount(): boolean {
-    /**
-     * Checks whether an account can be creeated with the information inputed
-     * Returns true and logs the user in if it suceeds
-     * Returns false and shows an error message if it fails
-    */
-    // if both fields are filled in, the user name is unique, and password meets requirements(?), 
-      // then create the new account and log the user
-      // return true
+  /**
+   * Creates a new account and logs user in
+   * If account creation fails, an error message is displayed
+   * @param username the username of the new account entered by the user
+   * @param password the password for the new account entered by the user, must be at least 8 characters long
+   * @returns true if the account was created and false if it was not
+   */
+  createAccount(username: String, password: String): boolean {
+    // if both fields are filled in and password meets requirements, 
+    if (username != "" && password.length >= 8) {
+      // then try to create the new account
+      if (this.userService.createAccount(username, password)) {
+        // log in user
+        return true;
+      // if creating the account fails (username not unique), set appropiate error message
+      } else {
+        this.errorMessage = "This username was already taken.";
+      }
     // use else if statements to reveal an errormessage that describes the problem
-    this.errorMessage = "This is a placeholder error message";
+    } else if (username == "") {
+      this.errorMessage = "You must enter a username.";
+    } else if (password.length < 8) {
+      this.errorMessage = "Your password must be at least 8 characters long."
+    }
     this.display = "initial";
-      // return false
     return false;
   }
 
+  /**
+   * Logs the user into their account and brings them to their home screen (buyer or admin)- might need parameter
+   */
   login(): void {
-    // logs the user into their account and brings them to their home screen (buyer or admin)- might need parameter
+    
   }
 
 }
--- estore-ui/src/app/user.service.ts
+++ estore-ui/src/app/user.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private usersURL = 'http://localhost:8080';
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  createAccount(username: String, password: String): boolean {
+    return false;
+  }
+
 }

aabf11f3f9c9d2d577647b038dfde8c086f5acbf
unknown <bhm6652@rit.edu>
2022-10-04 17:09:07 -0400

    switching branches

--- estore-api/src/main/java/com/estore/api/estoreapi/controller/UserController.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/controller/UserController.java
@@ -16,8 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.estore.api.estoreapi.UserDAO;
 import com.estore.api.estoreapi.model.User;
+import com.estore.api.estoreapi.persistance.UserDAO;
 
 @RestController
 @RequestMapping("users")

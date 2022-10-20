c0fa7f3f2c5e9971d636a90ed6298602d9f9ce2f
Peyton Wagner <paw7341@g.rit.edu>
2022-10-19 22:02:16 -0400

    added to login component

--- estore-ui/src/app/login/login.component.ts
+++ estore-ui/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 import { User } from '../user'
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,18 +13,25 @@ export class LoginComponent implements OnInit {
   display = "none";
   user: User = {username: "", password: ""};
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   login(username: String, password: String) {
     this.userService.loginUser(username, password).subscribe(user => {this.user = user;
-      if (this.user.username != "") {
-        if (this.user.username == "admin") {
-          ///
+      if (username != "") {
+        if (username == "admin") {
+            this.router.navigate(['admin-homepage'])
+        }
+        else{
+          this.router.navigate(['user-homepage'])
         }
       }
+      else{
+        this.errorMessage = "Username and/or password is incorrect.";
+      }
     });
+    this.display = "initial";
   }
 }

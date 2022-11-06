21849c3bf0d37c9105e55612bee559d04ca45aea
SpazzStuff <36232417+SpazzStuff@users.noreply.github.com>
2022-11-06 12:55:24 -0500

    notes

--- estore-ui/src/app/shopping-cart/shopping-cart.component.ts
+++ estore-ui/src/app/shopping-cart/shopping-cart.component.ts
@@ -12,26 +12,30 @@ import { User } from '../user'
 })
 export class ShoppingCartComponent implements OnInit {
   
-  user: User | undefined;
+  user: User | undefined; // honestly probably easier to just get and store the username
+  // should have a products/cart field
 
   constructor(private route: ActivatedRoute, private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
-    this.getShoppingCart
+    // call get user and set the field
+    this.getShoppingCart  // not currently calling the function (no parentheses)
   }
 
   getUser(){
-    const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
+      // I think its the username at the end of the url not the id
+    const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10); 
     this.userService.getUser(id)
       .subscribe(user => this.user = user);
   }
 
-  getShoppingCart(user: User){
-    this.userService.getCart(user.username).subscribe()
+  getShoppingCart(user: User){  // shouldn't need paremeter if its a field
+    this.userService.getCart(user.username).subscribe(/*products -> this.products = products*/)
+      // do something like the comment in subscribe to set the products/cart field once you get it from the service
   }
 
   checkout(user: User): void{
-      if (this.userService.getCart(user.username) != null){
+      if (this.userService.getCart(user.username) != null){ //should be able to just check the products/cart field rather than calling the service again
         this.router.navigate(['checkout-page']);
       }
       // probably want some sort of message to appear if there are no products

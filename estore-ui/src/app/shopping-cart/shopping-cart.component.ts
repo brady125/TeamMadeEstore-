e19e4497eecfcdbc6eec5a518167ebdc4a6daf22
SpazzStuff <36232417+SpazzStuff@users.noreply.github.com>
2022-11-06 12:32:33 -0500

    notes

--- estore-ui/src/app/shopping-cart/shopping-cart.component.ts
+++ estore-ui/src/app/shopping-cart/shopping-cart.component.ts
@@ -13,16 +13,23 @@ import { User } from '../user'
 export class ShoppingCartComponent implements OnInit {
 
   products: Product[] = []
+  // probably want a username field
 
   constructor(private userService: UserService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    // get username from url (you can check the user homepage for reference)
+    // use userService to get the user's shopping cart and assign to products field
+      // will need to add this method to UserService
   }
 
-  checkout(products: Product[], user: User): void{
+  checkout(products: Product[], user: User): void{  // if products and username are both fields you don't need as parameters
       if (products.length > 0){
         this.router.navigate(['checkout-page/'+user.username]);
       }
+      // probably want some sort of message to appear if there are no products
+      // or could have the checkout button be disabled by default and then enable it in ngOnInit 
+        // if there are products in the shopping cart
   }
 
 }

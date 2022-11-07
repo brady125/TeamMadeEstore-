242f59da90670b091016fc168ccfbc7788efa3cd
Peyton Wagner <paw7341@g.rit.edu>
2022-11-06 22:03:05 -0500

    fixed some shopping cart and checkout errors

--- estore-ui/src/app/shopping-cart/shopping-cart.component.ts
+++ estore-ui/src/app/shopping-cart/shopping-cart.component.ts
@@ -12,39 +12,36 @@ import { User } from '../user'
 })
 export class ShoppingCartComponent implements OnInit {
 
-  // username: string = ""-- since the shopping cart is stored in the user it's probably better to go back to storing the whole User my bad
   user!: User
   products: Product[] = []
+  errorMessage = "";
+  display = "none";
 
-  constructor(private route: ActivatedRoute, private userService: UserService, private productService: ProductService, private router: Router) { }
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
-    // call get user and set the field
-    this.getUser()
-    this.getShoppingCart()
+    this.getUser();
+    this.getShoppingCart();
   }
 
   getUser(){
-    // const id = parseInt(this.route.snapshot.paramMap.get('username')!, 10);-- changed to get username since the username is part of the path not the id
     const username = this.router.url.split("/").pop()!
     this.userService.getUser(username)
       .subscribe(user => this.user = user);
   }
 
   getShoppingCart(){
-    // this.userService.getCart(this.user.username).subscribe(p => this.products = p)
-      // do something like the comment in subscribe to set the products/cart field once you get it from the service
-    // scrach all that it's already in the user
-    this.products = this.user.shoppingCart
+    this.products = this.user.shoppingCart;
   }
 
   checkout(): void{
       if (this.products.length > 0){
         this.router.navigate(['checkout-page']);
       }
-      // probably want some sort of message to appear if there are no products
-      // or could have the checkout button be disabled by default and then enable it in ngOnInit 
-        // if there are products in the shopping cart
+      else{
+        this.errorMessage = "no items";
+        this.display = "initial";
+      }
   }
 
 }
